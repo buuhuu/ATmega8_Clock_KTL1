873c655d90268f02d2cc0d8dd2b8335ba85807cf
ONeill <onl@onl-laptop.(none)>
2011-02-21 21:22:09 +0100

    added functions to draw geometry (untested)

---
+++ output/geometry.c
@@ -0,0 +1,89 @@
+/*
+Copyright (C) 2011 Paul Ritter
+
+Dieses Programm ist freie Software. Sie können es unter den Bedingungen der GNU
+General Public License Version 2, wie von der Free Software Foundation veröffentlicht,
+weitergeben und/oder modifizieren.
+
+Die Veröffentlichung dieses Programms erfolgt in der Hoffnung, daß es Ihnen von Nutzen
+sein wird, aber OHNE IRGENDEINE GARANTIE, sogar ohne die implizite Garantie der
+MARKTREIFE oder der VERWENDBARKEIT FÜR EINEN BESTIMMTEN ZWECK. Details finden Sie in
+der GNU General Public License.
+
+Sie sollten ein Exemplar der GNU General Public License zusammen mit diesem Programm
+erhalten haben. Falls nicht, schreiben Sie an die Free Software Foundation, Inc., 51
+Franklin St, Fifth Floor, Boston, MA 02110, USA.
+*/
+
+#include <avr/io.h>
+#include <inttypes.h>
+
+#include "display_12_10.h"
+#include "geometry.h"
+
+struct point_t point(uint8_t col, uint8_t row) {
+    struct point_t temp;
+    temp.column = col;
+    temp.row = row;
+    return temp;
+};
+
+uint8_t min(uint8_t a, uint8_t b) {
+	return (a < b) ? a : b;
+};
+
+uint8_t max(uint8_t a, uint8_t b) {
+	return (a > b) ? a : b;
+};
+
+void drawLine(bitmap_t dest, struct point_t start, struct point_t end) {
+    uint8_t i, j;
+	/* care about vertical lines */
+	if (start.column == end.column) {
+		for (i=start.row; i <= end.column; i++) {
+			dest[i] |= (1 << start.column);
+		};
+		return;
+	};
+
+	/* care about horizontal lines */
+	if (start.row == end.row) {
+		dest[start.row] |= (~(1 << start.column) << end.column);
+		return;
+	};
+
+	/* care about other types of lines and generate steps */
+	uint8_t step;
+	uint8_t current;
+
+	if ((start.row - end.row) > (start.column - end.column)) {
+		step = (start.row - end.row) / (start.column - end.column);
+		current = start.row;
+		for (i = start.column; i <= end.column; i++) {
+			for (j=step; j > 0; j--) {
+				dest[min(current++,end.row)] |= 1 << i;
+			};
+		};
+	} else {
+		step = (start.column - end.column) / (start.row - end.row);
+		current = start.column;
+		for (i = start.row; i <= end.row; i++) {
+			dest[i] |= (~(1 << step) << current);
+			current += step;
+		};
+	};
+};
+
+inline void swap(uint8_t* left, uint8_t* right) {
+	uint8_t temp = *left;
+	*left = *right;
+	*right = temp;
+};
+
+void clearBitmap(bitmap_t* dest) {
+    int i;
+	for (i=0; i < 10; i++) {
+		(*dest)[i] = 0x0;
+	};
+};
+
---
+++ output/geometry.h
@@ -0,0 +1,39 @@
+/*
+Copyright (C) 2011 Paul Ritter
+
+Dieses Programm ist freie Software. Sie können es unter den Bedingungen der GNU
+General Public License Version 2, wie von der Free Software Foundation veröffentlicht,
+weitergeben und/oder modifizieren.
+
+Die Veröffentlichung dieses Programms erfolgt in der Hoffnung, daß es Ihnen von Nutzen
+sein wird, aber OHNE IRGENDEINE GARANTIE, sogar ohne die implizite Garantie der
+MARKTREIFE oder der VERWENDBARKEIT FÜR EINEN BESTIMMTEN ZWECK. Details finden Sie in
+der GNU General Public License.
+
+Sie sollten ein Exemplar der GNU General Public License zusammen mit diesem Programm
+erhalten haben. Falls nicht, schreiben Sie an die Free Software Foundation, Inc., 51
+Franklin St, Fifth Floor, Boston, MA 02110, USA.
+*/
+
+#ifndef GEOMETRY_H_INCLUDED
+#define GEOMETRY_H_INCLUDED
+
+/* point */
+struct point_t {
+	uint8_t row;
+	uint8_t column;
+};
+
+struct point_t point(uint8_t col, uint8_t row);
+
+uint8_t min(uint8_t a, uint8_t b);
+
+uint8_t max(uint8_t a, uint8_t b);
+
+void drawLine(bitmap_t dest, struct point_t start, struct point_t end);
+
+inline void swap(uint8_t* left, uint8_t* right);
+
+void clearBitmap(bitmap_t* dest);
+
+#endif
