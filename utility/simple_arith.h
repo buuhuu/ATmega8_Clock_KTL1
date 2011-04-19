bdbea92a0b1169a3fcd2c81fc25418cc37a7879d
Lucas Stach <dev@lynxeye.de>
2011-04-20 01:18:16 +0200

    some more const (d'oh)

--- utility/simple_arith.h
+++ utility/simple_arith.h
@@ -27,29 +27,29 @@ Franklin St, Fifth Floor, Boston, MA 02110, USA.
 /**
  * min_{type} returns the smaller one of two values
  */
-static inline uint8_t min_u8(uint8_t a, uint8_t b) {
+static inline uint8_t min_u8(const uint8_t a, const uint8_t b) {
 	return (a < b) ? a : b;
 }
 
-static inline int8_t min_s8(int8_t a, int8_t b) {
+static inline int8_t min_s8(const int8_t a, const int8_t b) {
 	return (a < b) ? a : b;
 }
 
 /**
  * max_{type} returns the bigger one of two values
  */
-static inline uint8_t max_u8(uint8_t a, uint8_t b) {
+static inline uint8_t max_u8(const uint8_t a, const uint8_t b) {
 	return (a > b) ? a : b;
 }
 
-static inline int8_t max_s8(int8_t a, int8_t b) {
+static inline int8_t max_s8(const int8_t a, const int8_t b) {
 	return (a > b) ? a : b;
 }
 
 /**
  * abs_{type} returns the value of a value witout regard to it's sign
  */
-static inline int8_t abs_s8(int8_t a) {
+static inline int8_t abs_s8(const int8_t a) {
     return (a < 0) ? -a : a;
 }
 
